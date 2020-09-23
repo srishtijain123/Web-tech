477bf4ec68e09011762a6a0b6750e8465bc3e5e4
jasmine <39827651+jaassmine@users.noreply.github.com>
2020-09-23 22:48:54 +0530

    Initila Commit

---
+++ IT service/main.js
@@ -0,0 +1,25 @@
+const faders = document.querySelectorAll('.fade-in');
+
+
+const appearOptions = {
+  threshold: 1, 
+  rootMargin: "0px 0px -100px 0px"
+} ;
+   
+const appearOnScroll = new IntersectionObserver(
+  function(entries , apperarOnScoll) {
+    entries.forEach(entry => {
+      if(!entry.isIntersecting){
+        return ;
+      }
+      else{
+        entry.target.classList.add('appear');
+        appearOnScroll.unobserve(entry-target) ;
+      }
+    });
+  }, appearOptions);
+
+
+faders.forEach(fader => {
+  appearOnScroll.observe(fader);
+})
